8a76416207beb6b69d5f93f74518ce1cf3077320
José Alberto dos Santos Junior <alberto.junior.si@gmail.com>
2022-02-06 15:58:10 -0300

    att

--- exercicio02 - javascript/script.js
+++ exercicio02 - javascript/script.js
@@ -2,5 +2,9 @@ var Data = new Date()
 var hora = Data.getHours()
 
 var horario = document.getElementById('horario')
+var imagem = document.getElementsByClassName('StyleDiv')
+
+imagem.setAttribute("style", "background-image: url(img/tarde.jpg)")
 
 horario.innerHTML = `Agora são ${hora} horas.`
+
